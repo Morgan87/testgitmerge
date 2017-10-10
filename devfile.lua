bd264739d6f6c46bacf84b1d1ed8b7242973e121
林毅 <linyi@linyideMac-mini.local>
2017-10-10 10:52:34 +0800

    公共节点1

--- devfile.lua
+++ devfile.lua
@@ -2,3 +2,14 @@
 -- Author: LY
 -- Date: 2017-10-10 10:26:06
 --
+#include <stdio.h>
+#include <unistd.h>
+
+void base_func() {
+	int *p;
+	printf("this is a crash %d\n", *p);
+}
+
+int main() {
+	return 0;
+}
