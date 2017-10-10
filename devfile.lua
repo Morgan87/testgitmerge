8825babe598ea35141ae4c0f8408ee8c60504e93
林毅 <linyi@linyideMac-mini.local>
2017-10-10 11:01:01 +0800

    再次重来

--- devfile.lua
+++ devfile.lua
@@ -1,19 +1,12 @@
---
--- Author: LY
--- Date: 2017-10-10 10:26:06
---
 #include <stdio.h>
 #include <unistd.h>
 
 void base_func() {
-	int a = 1;
-	int *p = &a;
+	int *p;
 	printf("this is a crash %d\n", *p);
-}
-void dev_func_1() {
-	printf("dev func 1\n");
 }
 
 int main() {
 	return 0;
 }
+
