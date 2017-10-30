6e665844f8ffde76e2a260a1e44904d9139c5032
林毅 <linyi@linyideMac-mini.local>
2017-10-30 17:39:31 +0800

    提交 1

--- devfile.lua
+++ devfile.lua
@@ -25,3 +25,14 @@ int main() {
 	return 0;
 }
 
+--联盟总动员
+    allianceTaskView                = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskView",
+    allianceTaskItem                = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskItem",
+    allianceTaskItemDetail          = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskItemDetail",
+    allianceTaskLogItem             = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskLogItem",
+    allianceTaskGroupView           = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskGroupView",
+    allianceTaskLogScrollView       = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskLogScrollView",
+    allianceTaskRankView            = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskRankView",
+    allianceTaskResult              = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskResult",
+    allianceTaskRewrad              = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.allianceTaskRewrad",
+    commonInstruction               = CURRENT_LUAFILE_PATH .. ".game.allianceView.allianceTaskView.commonInstruction",
